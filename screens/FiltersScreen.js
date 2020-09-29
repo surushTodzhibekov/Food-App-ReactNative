57faa3a86168732bbef9f19ee2f9075bb230c595
YousefAlhamed <yousefalhamed7@gmail.com>
2020-09-29 23:18:41 +0300

    Adding and Styling Filter screen content

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,72 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Switch, Text, Platform } from "react-native";
+import Colors from "../constants/Colors";
+
+const FilterSwitch = ({ title, value, onValueChange }) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{title}</Text>
+      <Switch
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={Platform === "android" ? Colors.primaryColor : ""}
+        value={value}
+        onValueChange={onValueChange}
+      />
+    </View>
+  );
+};
 
 function FiltersScreen(props) {
-  return <View style={styles.container}></View>;
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactous, setIsLactous] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegatarian, setIsVegatarian] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        title="Gluten-free"
+        value={isGlutenFree}
+        onValueChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        title="Lactous-free"
+        value={isLactous}
+        onValueChange={(newValue) => setIsLactous(newValue)}
+      />
+      <FilterSwitch
+        title="Vegan"
+        value={isVegan}
+        onValueChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        title="Vegatarian"
+        value={isVegatarian}
+        onValueChange={(newValue) => setIsVegatarian(newValue)}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    marginVertical: 30,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "90%",
+    marginVertical: 15,
+  },
 });
 
 export default FiltersScreen;
